9a3b44257265c33cbd802ed32d6f1155fd0d48ad
Kyeonghui <dkdk177@naver.com>
2022-02-03 13:26:02 +0900

    2장 완성

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index,detail,answer_create
+from .views import index,detail,answer_create,question_create
 
 app_name = 'pybo'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', index,name='index'),
     path('<int:question_id>/',detail,name='detail'),
     path('answer/create/<int:question_id>/',answer_create, name='answer_create'),
+    path('question/create/', question_create, name='question_create'),
 ]
