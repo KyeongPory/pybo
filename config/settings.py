d0bda7155a7d0f3da5505c50b1c3218490e3fb54
Kyeonghui <dkdk177@naver.com>
2022-02-04 15:31:25 +0900

    로그인,로그아웃,회원가입 완성

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.35.99']
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Application definition
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pybo',
+    'common',
 ]
 
 MIDDLEWARE = [
